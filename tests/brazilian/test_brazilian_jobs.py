d75cc4eab145eb717f27eb52326295cc6438fed0
Sass <dudu_sass12@hotmail.com>
2022-08-26 18:09:13 -0300

    brazilian_test

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    el = jobs[-1]
+    assert el == {
+        "title": "Auxiliar de manutenção",
+        "salary": ' 1400',
+        "type": ' full time',
+    }
